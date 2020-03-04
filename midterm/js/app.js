235f3ffc233ae7644442958d085d33c4cb47c041
EC2 Default User <ec2-user@ip-172-31-33-29.ec2.internal>
2020-03-04 23:46:20 +0000

    Added midterm stuff

--- midterm/js/app.js
+++ midterm/js/app.js
@@ -1,13 +1,12 @@
-let buildGallery = function(){
-	$("#images").empty();
-	var url = "https://dog.ceo/api/breeds/image/random/" + $("#number-images").val();
+let searchBook = function(){
+	var url = `https://https://openlibrary.org/dev/docs/api/books?bibkeys=ISBN:${$('#isbn').val()}`;
 	$.ajax({
 		url: url,
 		type: "GET",
 		dataType: "json",
 		success: function(data){
 			data.message.forEach(function(src){
-				$("#images").append("<div class=\"img-thumbnail flex-item\"><img src=\""+src+"\"></div>");
+				
 			});
 		},
 		error: function(err){
