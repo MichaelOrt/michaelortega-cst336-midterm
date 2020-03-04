ba80c77aeafdb9f7b487b5247457605824cec790
EC2 Default User <ec2-user@ip-172-31-33-29.ec2.internal>
2020-03-04 23:53:03 +0000

    Added midterm stuff

--- midterm/js/app.js
+++ midterm/js/app.js
@@ -1,13 +1,20 @@
 let searchBook = function(){
-	var url = `https://https://openlibrary.org/dev/docs/api/books?bibkeys=ISBN:${$('#isbn').val()}`;
+	var url = `https://openlibrary.org/api/books?bibkeys=ISBN:${$('#isbn').val()}`;
+
 	$.ajax({
 		url: url,
 		type: "GET",
 		dataType: "json",
 		success: function(data){
-			data.message.forEach(function(src){
-				
-			});
+			var key = 'ISBN:'+$('#isbn').val();
+			console.log(data[key]);
+			$('.cover').append(`<img src="${data[key].cover.medium}">`);
+			$('.info').append(`<p >Title:${data[key].title}</p>`);
+			$('.info').append(`<p >Author:${data[key].authors.name}</p>`);
+			$('.info').append(`<p >Publish year:${data[key].pubish_date}</p>`);
+			$('.info').append(`<p >Publisher:${data[key].publishers[0].name}</p>`);
+			$('.info').append(`<p >ISBN:${$('#isbn').val()}</p>`);
+			$('.info').append(`<p >Pages:${data[key].title}</p>`);
 		},
 		error: function(err){
 			console.log(err);
